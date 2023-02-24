9a8c41c432b0afbec41efdf3e98fc42fd946a38d
AbuTech <Idsrari@outlook.com>
2023-02-24 00:14:23 -0700

    Completed order insert post and tested endpoints

--- endpoints/order.py
+++ endpoints/order.py
@@ -50,27 +50,29 @@ def get_order_restaurant():
 # The POST for this endpoint allows creating orders. Orders can only contain items from a single restaurant and can only be placed by signed-in clients
 # The items in an order are a mandatory field and must be in list form. The list of items corresponds to the menu item IDs
 # Since its only signed in clients i think it might be a clientToken that is required. Need to confirm 
-# Only did is-cancelled input because it was a client side insert and clients can only cancel an order
+# Only did is-cancelled input because it was a client side insert and clients can only cancel an order. Looks like data truncation issue affects all columns too 
 # Convert the True/False str boolean to an int as my "isCancelled" column is of TinyInt(1) which is an integer 0/1 representing True or False in my stored procedure
 @app.post('/api/order')
 def insert_order():
-    required_data = ['restaurantId', 'items']
+    keys = ['orderId', 'clientId', 'restaurantId', 'isConfirmed', 'isComplete', 'isCancelled', 'createdAt', 'items']
+    # cut out items for now in required data
+    required_data = ['restaurantId']
     check_result = check_data(request.json, required_data)
     if check_result != None:
         return check_result
     token_input = request.headers.get("clientToken")
-    # is_cancelled_input = request.json.get('isCancelled')
     client_id_input = request.json.get("clientId")
     restaurant_id_input= request.json.get('restaurantId')
-    item_input = ",".join(map(str, request.json.get('items', [])))
-    result = run_statement("CALL insert_order_arg_tkresid(?,?,?,?)", [token_input, client_id_input, restaurant_id_input, item_input])
-    if result == None:
-        return make_response(jsonify("Order inserted successfully"), 200)
+    item_input = request.json.get('items')
+    result = run_statement("CALL create_order(?,?,?)", [token_input, client_id_input, restaurant_id_input])
+    # This basically will extract the result generated which is a list of lists of order id Ex. [[23,]]
+    order_id = result[0][0] 
+    if (type(result) == list):
+        for item in item_input:
+            result = run_statement("CALL insert_order_item(?,?)", [item, order_id]) 
+        return make_response(jsonify("Order-id selected and items list inserted successfully"), 200)
     else:
-        return make_response(jsonify("Failed to insert order. Something went wrong"), 500)
-        # print(is_cancelled_input, type(is_cancelled_input))
-                
-# THIS IS NONSENSE. DATA TRUNCATION ERRORS EVERYWHERE
+        return make_response(jsonify(result), 500)
 
 # Client Order Edit
 @app.patch('/api/order-client')
